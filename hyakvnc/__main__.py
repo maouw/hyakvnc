3d5915efd26787d7ccde3ea505acc009910ff3c9
Altan Orhon <altan@uw.edu>
2023-09-21 16:37:58 -0700

    Cleaned up

--- hyakvnc/__main__.py
+++ hyakvnc/__main__.py
@@ -159,9 +159,10 @@ def cmd_stop(job_id: Optional[int] = None, stop_all: bool = False):
 
 
 def cmd_status():
-    logger.info("Finding running VNC jobs")
+    logger.info("Finding running VNC jobs...")
     vnc_instances = HyakVncInstance.find_running_instances(instance_prefix=app_config.apptainer_instance_prefix,
                                                            apptainer_config_dir=app_config.apptainer_config_dir)
+    logger.info("Found {len(vnc_instances)} running VNC jobs:")
     for instance in vnc_instances:
         print(f"Instance {instance.apptainer_instance_info.instance_name} running as SLURM job {instance.job_id} with port {instance.vnc_port}")
 
