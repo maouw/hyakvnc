7dc4596855ba83e6d1cf836f279166ec7e2e56ea
Altan Orhon <altan@uw.edu>
2023-09-28 14:53:35 -0700

    Added a little waiting back

--- hyakvnc/__main__.py
+++ hyakvnc/__main__.py
@@ -171,6 +171,8 @@ def cmd_create(container_path: Union[str, Path], dry_run=False):
 
     logger.info("Waiting for Apptainer instance to start running")
     if wait_for_file(str(instance_file), timeout=app_config.sbatch_post_timeout):
+        time.sleep(2)
+        logger.info("Apptainre instance started running. Waiting for VNC session to start")
 
         def get_session():
             try:
